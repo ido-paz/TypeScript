69af76b653326fae144b0441b48ba18c662a7308
ido-paz <ido_p_a@hotmail.com>
2019-11-05 15:53:42 +0200

    changed ' with "

--- src/basics.ts
+++ src/basics.ts
@@ -2,7 +2,7 @@ let now : Date = new Date();
 let second : number = now.getSeconds() ;
 let minute : number = now.getMinutes() ;
 let hour : number = now.getHours() ;
-const message : string = 'execution started at: ' + hour + ':' + minute + ':' + second;
+const message : string = "execution started at: " + hour + ":" + minute + ":" + second;
 //
-const logger = new Logger(document.querySelector('#outputDiv'));
+const logger :Logger = new Logger(document.querySelector("#outputDiv"));
 logger.Append2Div(message);
