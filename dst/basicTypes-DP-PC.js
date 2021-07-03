c64c3fda62f4eee66c2b18f06f76993e312c2812
ido_p_a@hotmail.com <ido_p_a@hotmail.com>
2021-07-03 15:42:06 +0300

    merge

---
+++ dst/basicTypes-DP-PC.js
@@ -0,0 +1,21 @@
+var MAAM = 1.17;
+var hasMoney = true;
+var message = "good evening";
+var changable = 'a';
+var names = ['Ido', 'Yarden', 'Moshe', 'Shani'];
+var today = new Date();
+//
+var Color;
+(function (Color) {
+    Color[Color["red"] = 0] = "red";
+    Color[Color["green"] = 1] = "green";
+})(Color || (Color = {}));
+var Languages;
+(function (Languages) {
+    Languages["he"] = "he";
+    Languages["en"] = "en";
+})(Languages || (Languages = {}));
+var c = Color.red;
+//
+changable = 1;
+//
