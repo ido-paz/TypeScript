833113b02d8a5df64dc16d27fb8c621b000539c9
ido_p_a@hotmail.com <ido_p_a@hotmail.com>
2021-07-08 13:03:13 +0300

    add interface demo

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,28 +1,27 @@
-interface IUser{
-    name:string,
-    age:number
+interface IUser {
+  name: string;
+  age: number;
 }
 
-checkUser({name:'a',age:14});
-
-function checkUser(user : IUser){
-
-}
-
-interface IPlayer{
-    model:string;
-    play:Function;
-    stop:Function;
+interface IPlayer {
+  model: string;
+  play: Function;
+  stop: Function;
 }
 //
-class Player implements IPlayer{
-    model:string;
-    //
-    play(){
-        console.log(`${this.model} started playing`);
-    }
-    //
-    stop(){
-        console.log(`${this.model} stopped playing`);
-    }
+class Player implements IPlayer {
+  model: string;
+  //
+  play() {
+    console.log(`${this.model} started playing`);
+  }
+  //
+  stop() {
+    console.log(`${this.model} stopped playing`);
+  }
 }
+//
+function checkUser(user: IUser) {
+  console.log(user);
+}
+checkUser({ name: "a", age: 14 });
